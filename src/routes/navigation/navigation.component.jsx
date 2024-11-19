e820cba6e68dd4c7854cec59cf41f1a75672f583
Csaba Klein <sympla@gmail.com>
2024-11-19 20:33:07 +0100

    Update Navigation

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,21 @@
+import { Fragment } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigaton">
+        <Link className="navigation">
+          <div>Logo</div>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+export default Navigation;
