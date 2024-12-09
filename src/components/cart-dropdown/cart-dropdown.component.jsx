96b607439e2c78639681af4fc2b1cc01b5d3d5f7
Csaba Klein <sympla@gmail.com>
2024-12-09 21:17:23 +0100

    Add Cart functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,20 @@
 import './cart-dropdown.styles.scss';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  console.log(cartItems);
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        <Button>Checkout</Button>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
       </div>
+      <Button>Checkout</Button>
     </div>
   );
 };
