d41dd499c33f1d82a34a5602fdaf114e9bcbc56b
Csaba Klein <sympla@gmail.com>
2025-02-25 17:16:28 +0100

    Add Stripe to app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './index.scss';
+import { Elements } from '@stripe/react-stripe-js';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
@@ -6,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import { persistor, store } from './store/store';
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 const rootElement = document.getElementById('root');
 
@@ -14,7 +16,9 @@ render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
